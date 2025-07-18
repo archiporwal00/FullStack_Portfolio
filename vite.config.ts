c9d3cdcbadac41e5c3daf356506510e6784b24a3
Archi Porwal <porwalarchi00@gmail.com>
2025-07-18 18:04:44 +0530

    Fix: Correct base path and PWA scope for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType:"autoUpdate",
-      manifest:{
-        name: "Rishi Bakshi's Portfolio",
-        short_name: "Rishi's Portfolio",
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Archi Porwal's Portfolio",
+        short_name: "Archi's Portfolio",
         theme_color: "#000000",
         background_color: "#000000",
         display: "standalone",
-        scope: "/portfolio",
-        start_url: "/portfolio",
+        scope: "/",
+        start_url: "/",
         icons: [
           {
             src: "maskable.png",
@@ -44,8 +43,9 @@ export default defineConfig({
             type: "image/png",
           },
         ],
-      }
-    })
+      },
+    }),
   ],
-  base:"/portfolio/",
+
+  base: '/',
 })
